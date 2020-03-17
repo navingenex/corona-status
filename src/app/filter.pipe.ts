5f28e626e488928f8bf85471e66e7ce1db22bf51
navingenex <navingenex@gmail.com>
2020-03-17 23:00:24 +0530

    okey

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,12 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  transform(value: any, args: string): unknown {
     if(!args)
     return value;
     if(!value)
     return null
-    return value.filter((f:any)=>f.country.toLowerCase().includes(args))
+    return value.filter((f:any)=>f.country.toLowerCase().includes(args.toLowerCase()))
   }
 
 }
